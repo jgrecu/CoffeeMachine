12c0bb83b8f16d4c045fcfa1982b6712ec927bcf
Jeremy Grecu <jeremy.grecu@sap.com>
2021-07-10 14:31:00 +0100

    JBA - Coffee Machine - Stage 5/6: Keep track of the supplies

--- task/src/machine/CoffeeMachine.java
+++ task/src/machine/CoffeeMachine.java
@@ -12,41 +12,44 @@ public class CoffeeMachine {
 
     public static void main(String[] args) {
 
-        printStatus();
-        System.out.println("Write action (buy, fill, take): ");
-        String action = scanner.nextLine();
-        switch (action) {
-            case "buy":
-                buy();
-                scanner.close();
-                break;
-            case "fill":
-                fill();
-                scanner.close();
-                break;
-            case "take":
-                take();
-                scanner.close();
-                break;
-            default:
-                scanner.close();
-                break;
+        //printStatus();
+        boolean exitStatus = false;
+        while(!exitStatus) {
+            System.out.println("Write action (buy, fill, take, remaining, exit): ");
+            String action = scanner.nextLine();
+            switch (action) {
+                case "buy":
+                    buy();
+                    break;
+                case "fill":
+                    fill();
+                    break;
+                case "take":
+                    take();
+                    break;
+                case "remaining":
+                    printStatus();
+                    break;
+                case "exit":
+                    exitStatus = true;
+                    break;
+                default:
+                    break;
+            }
         }
-        printStatus();
-
     }
 
     private static void buy() {
-        System.out.println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino: ");
-        int choice = scanner.nextInt();
+        System.out.println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu: ");
+        String choice = scanner.nextLine();
         switch (choice) {
-            case 1:
+            case "1":
                 buyEspresso();
                 break;
-            case 2:
+            case "2":
                 buyLatte();
                 break;
-            case 3:
+            case "3":
                 buyCappuccino();
                 break;
             default:
@@ -57,7 +60,7 @@ public class CoffeeMachine {
 
 
     public static void printStatus() {
-        System.out.println("The coffee machine has:\n" +
+        System.out.println("\nThe coffee machine has:\n" +
                 waterSupply + " ml of water\n" +
                 milkSupply + " ml of milk\n" +
                 beansSupply + " g of coffee beans\n" +
@@ -69,11 +72,18 @@ public class CoffeeMachine {
         int waterPerCoffee = 250;
         int beansPerCoffee = 16;
         int cost = 4;
-        if (waterSupply > waterPerCoffee && beansSupply > beansPerCoffee && cupsSupply > 1) {
+        if (waterSupply >= waterPerCoffee && beansSupply >= beansPerCoffee && cupsSupply >= 1) {
+            System.out.println("I have enough resources, making you a coffee!\n");
             waterSupply -= waterPerCoffee;
             beansSupply -= beansPerCoffee;
             money += cost;
             cupsSupply--;
+        } else if (waterSupply < waterPerCoffee) {
+            System.out.println("Sorry, not enough water!\n");
+        } else if (beansSupply < beansPerCoffee) {
+            System.out.println("Sorry, not enough coffee beans!\n");
+        } else {
+            System.out.println("Sorry, not enough cups!\n");
         }
     }
 
@@ -83,12 +93,21 @@ public class CoffeeMachine {
         int beansPerCoffee = 20;
         int cost = 7;
 
-        if (waterSupply > waterPerCoffee && beansSupply > beansPerCoffee && milkSupply > milkPerCoffee && cupsSupply > 1) {
+        if (waterSupply >= waterPerCoffee && beansSupply >= beansPerCoffee && milkSupply >= milkPerCoffee && cupsSupply >= 1) {
+            System.out.println("I have enough resources, making you a coffee!\n");
             waterSupply -= waterPerCoffee;
             beansSupply -= beansPerCoffee;
             milkSupply -= milkPerCoffee;
             money += cost;
             cupsSupply--;
+        } else if (waterSupply < waterPerCoffee) {
+            System.out.println("Sorry, not enough water!\n");
+        } else if (beansSupply < beansPerCoffee) {
+            System.out.println("Sorry, not enough coffee beans!\n");
+        } else if (cupsSupply < 1) {
+            System.out.println("Sorry, not enough cups!\n");
+        } else {
+            System.out.println("Sorry, not enough milk!\n");
         }
     }
 
@@ -98,12 +117,21 @@ public class CoffeeMachine {
         int beansPerCoffee = 12;
         int cost = 6;
 
-        if (waterSupply > waterPerCoffee && beansSupply > beansPerCoffee && milkSupply > milkPerCoffee && cupsSupply > 1) {
+        if (waterSupply >= waterPerCoffee && beansSupply >= beansPerCoffee && milkSupply >= milkPerCoffee && cupsSupply >= 1) {
+            System.out.println("I have enough resources, making you a coffee!\n");
             waterSupply -= waterPerCoffee;
             beansSupply -= beansPerCoffee;
             milkSupply -= milkPerCoffee;
             money += cost;
             cupsSupply--;
+        } else if (waterSupply < waterPerCoffee) {
+            System.out.println("Sorry, not enough water!\n");
+        } else if (beansSupply < beansPerCoffee) {
+            System.out.println("Sorry, not enough coffee beans!\n");
+        } else if (cupsSupply < 1) {
+            System.out.println("Sorry, not enough cups!\n");
+        } else {
+            System.out.println("Sorry, not enough milk!\n");
         }
     }
 
@@ -116,6 +144,7 @@ public class CoffeeMachine {
         beansSupply += scanner.nextInt();
         System.out.println("Write how many disposable cups of coffee you want to add: ");
         cupsSupply += scanner.nextInt();
+        scanner.nextLine();
     }
 
     private static void take() {
